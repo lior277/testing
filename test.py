8758d7049008d3e3b8675744d7723d7778674a5c
lior <lior277@gmail.com>
2025-05-15 11:44:30 +0300

    fix

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hellow')
+
+class FiltersPageUi():
+    def __init__(self) -> None:
+
+        def click_on_filter_by_type():
+            ff= 4*5
+            if(ff > 20):
+                print(ff)
+
